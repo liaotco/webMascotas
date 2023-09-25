2bd5653c9c745d13fdac21153744964376e76ab3
liaotc <rosalia.otero@gmail.com>
2023-09-25 14:01:12 +0200

    estilos

--- webMascotas/static/js/funciones.js
+++ webMascotas/static/js/funciones.js
@@ -71,49 +71,4 @@ function ventanaSecundaria (URL){
   window.open(URL,"Detalle Producto","width=520,height=300,scrollbars=NO")
 }
 
-/*CARRUSEL FOTOS*/
-/*let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-*/
-/*LOGIN*/
-let loginForm = document.querySelector(".my-form");
-
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let email = document.getElementById("email");
-    let password = document.getElementById("password");
-
-    console.log('Email:', email.value);
-    console.log('Password:', password.value);
-    // process and send to API 
-});
-function ventanaLogin (URL){
-  window.open(URL,"Login","width=520px,height=400px,scrollbars=YES")
-}
 
