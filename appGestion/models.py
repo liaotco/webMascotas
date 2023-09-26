3fd7e063621f690799ac37a64d9d10ccaf8eadc5
LiaOTCO <rosalia.otero@gmail.com>
2023-09-26 17:58:00 +0200

    cambio tablas

--- appGestion/models.py
+++ appGestion/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Producto(models.Model):
-    id = models.CharField(max_length=5,primary_key=True)
     nombre = models.CharField(max_length=50,blank=False)
     precio = models.FloatField()
     imagen=models.CharField(max_length=254,blank=True)
@@ -18,34 +17,28 @@ class Subscripcion(models.Model):
         return self.email
 
 class Departamento(models.Model):
-    dep_no=models.PositiveSmallIntegerField(primary_key=True)
-    dnombre=models.CharField(max_length=14,blank=False)
+    nombre=models.CharField(max_length=14,blank=False)
     localidad=models.CharField(max_length=30,blank=False)
 
 
 class Empleado(models.Model):
-    emp_no=models.PositiveSmallIntegerField(primary_key=True)
     apellido=models.CharField(max_length=8,blank=False)
     oficio=models.CharField(max_length=10,blank=False)
-    director=models.ForeignKey('self',on_delete=models.CASCADE,null=True)
     fecha_alta=models.DateField()
     salario=models.DecimalField(max_digits=8,decimal_places=2)
     comision=models.DecimalField(max_digits=8,decimal_places=2)
-    dep_no=models.ForeignKey(Departamento,on_delete=models.CASCADE,default='1',blank=True)
+    dep=models.ForeignKey(Departamento,on_delete=models.CASCADE,default='1',blank=True)
     telefono=models.CharField(max_length=14,blank=False)
 
 class Cliente(models.Model):
-    cliente_no=models.PositiveSmallIntegerField(primary_key=True)
     nombre=models.CharField(max_length=25,blank=False)
     localidad=models.CharField(max_length=14,blank=False)
-    vendedor_no=models.ForeignKey(Empleado,on_delete=models.CASCADE,blank=True)
     debe=models.DecimalField(max_digits=8,decimal_places=2)
     haber=models.DecimalField(max_digits=8,decimal_places=2)
     limite_credito=models.DecimalField(max_digits=8,decimal_places=2)
 
-class Pedido(models.Model):
-    pedido_no=models.PositiveSmallIntegerField(primary_key=True)
-    producto_no = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    cliente_no=models.ForeignKey(Cliente,on_delete=models.CASCADE)
+class Pedido(models.Model):   
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cliente=models.ForeignKey(Cliente,on_delete=models.CASCADE)
     unidades=models.SmallIntegerField()
     fecha_pedido=models.DateField()
